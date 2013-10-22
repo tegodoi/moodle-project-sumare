0c55dc835d2db21992e832ef2401666cf5cd09bb
rafaellucio <rafaelantoniolucio@gmail.com>
2013-10-22 12:11:06 -0200

    include new page

---
+++ client/app/scripts/login.js
@@ -0,0 +1,5 @@
+require(['jquery', 'bootstrap'], function ($, bootstrap) {
+	'use strict';
+
+	console.log('Running jQuery %s', $().jquery);
+});
--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,14 +1,27 @@
 require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery.min',
-        bootstrap: '../bower_components/bootstrap/docs/assets/js/bootstrap.min'
-    },
-    shim: { 'bootstrap': { deps: ['jquery'] } }
+	paths: {
+		jquery: '../bower_components/jquery/jquery.min',
+		bootstrap: '../bower_components/bootstrap/docs/assets/js/bootstrap.min'
+	},
+	shim: { 'bootstrap': { deps: ['jquery'] } },
+	exports: {
+		login: '../scripts/login'
+	}
 });
 
 require(['app', 'jquery', 'bootstrap'], function (app, $, bootstrap) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+	'use strict';
+	// use app here
+	// console.log(app);
+
+	$('#submitLogin').on('click', function () {
+		var user = $('#username'),
+			pass = $('#password');
+
+		if (user.val() === '' || pass.val() === '') {
+			alert('não tenho o  que faze');
+		} else {
+			window.location = '/';
+		}
+	});
 });
