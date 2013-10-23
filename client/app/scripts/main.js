b615932a72798172c657542eea25d0af63e822e8
rafaellucio <rafaelantoniolucio@gmail.com>
2013-10-22 23:09:33 -0200

    creating page new discipline

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -14,14 +14,29 @@ require(['app', 'jquery', 'bootstrap'], function (app, $, bootstrap) {
 	// use app here
 	// console.log(app);
 
+	var redirectPage = function () {
+		if (localStorage.getItem('redirect') === null) {
+			localStorage.setItem('redirect', 'true');
+			window.location.href = '/login.html';
+		};
+	}();
+
+	var sidebarActive = function () {
+		$('.sidebar-teacher ul li').on('click', function () {
+			if($(this).hasClass('nav-header')) return false;
+			$('.sidebar-teacher ul li').removeClass('active');
+			$(this).addClass('active');
+		});
+	}();
+
 	$('#submitLogin').on('click', function () {
 		var user = $('#username'),
 			pass = $('#password');
 
-		if (user.val() === '' || pass.val() === '') {
+		if (user.val() !== '' || pass.val() !== '') {
 			alert('não tenho o  que faze');
 		} else {
-			window.location = '/';
+			window.location = 'restricted_teacher.html';
 		}
 	});
 });
