33ed29c3d9beac636cdf19f6c03739a12626b1e7
rafaellucio <rafaelantoniolucio@gmail.com>
2013-10-23 00:12:00 -0200

    add othes layouts

---
+++ phpProject/access.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>moodle project sumare</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<!-- build:css(.tmp) styles/main.css -->
+	<link rel="stylesheet" href="styles/login.css">
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/docs/assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/docs/assets/css/bootstrap-responsive.css">
+	<style>
+		body {
+			padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+		}
+	</style>
+	<!-- endbuild -->
+
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="bower_components/bootstrap/docs/assets/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="bower_components/bootstrap/docs/assets/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="bower_components/bootstrap/docs/assets/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="bower_components/bootstrap/docs/assets/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="bower_components/bootstrap/docs/assets/ico/favicon.png">
+
+	<!-- build:js scripts/vendor/modernizr.js -->
+	<script src="bower_components/modernizr/modernizr.js"></script>
+	<!-- endbuild -->
+</head>
+<body>
+	<!--[if lt IE 10]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p> <![endif]-->
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="#">Project Moodle</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#about">Sobre</a></li>
+						<li><a href="#contact">Contato</a></li>
+					</ul>
+					<form action="check_users.php" method="post" name="form_login" id="form_login" class="navbar-form pull-right">
+						<input type="text" placeholder="Login" name="usuario" id="username" class="span2" />
+						<input type="password" placeholder="Senha" name="senha" id="password" class="span2" />
+						<button type="button" id="submitLogin" name="send" class="btn">Acessar</button>
+					</form>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<!-- Form Access -->
+		<div id="resposta"></div>
+		<form class="form-signin" action="register.php" method="POST" name="cadastro" id="cadastro">
+			<fieldset>
+				<legend style="padding:0 2px 0 0;">Cadastro de usuário</legend>
+				<div>
+					<label>Nome:</label>
+					<input class="input-block-level" style="width:300px;" type="text" placeholder="Nome" id="nome" name="nome" required />
+
+					<label>Sobrenome:</label>    
+					<input class="input-block-level" style="width:300px;" type="text" placeholder="Sobrenome" id="sobrenome" name="sobrenome" required />
+				</div>   
+				<label>E-mail:</label>
+				<input class="input-block-level" style="width:300px;" type="email" id="email" placeholder="example@example.com" name="email" required />
+
+				<label>Confirmação de e-mail:</label>
+				<input class="input-block-level" style="width:300px;" type="email" id="email_addr_repeat" name="email_addr_repeat" required />
+
+				<label>Login:</label>
+				<input class="input-block-level" style="width:300px;" type="text" placeholder="Login" name="usuario" id="usuario" required />
+				
+				<label>Informações:</label>
+				<textarea rows="3" style="width: 300px;" placeholder="Informações" name="info" id="info"></textarea>
+
+				<input type="hidden" id="nivel" name="nivel" value="<?php echo $id = $_GET['id']; ?>"/>
+
+				<label>Senha:</label>
+				<input class="input-block-level" style="width:300px;" type="password" placeholder="" id="senha" name="senha" required />
+
+				<label>Confirmação da senha:</label>
+				<input class="input-block-level" style="width:300px;" type="password" id="password_confimation" name="password_confirmation" required />
+				<br>
+				<button type="submit" id="send"class="btn">Enviar</button>
+			</fieldset>
+			<br>
+			<a class="a-right" href="restricted_area.html"> &laquo;Voltar</a>
+		</form>
+
+		<hr>
+
+		<footer>
+			<p>&copy; Objeto de Aprendizagem de Projeto 2013</p>
+		</footer>
+
+	</div> <!-- /container -->
+
+	<!-- build:js scripts/vendor.js -->
+	<!-- bower:js -->
+	<script src="bower_components/jquery/jquery.js"></script>
+	<!-- endbower -->
+	<!-- endbuild -->
+
+	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+	<script>
+		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='//www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		ga('create','UA-XXXXX-X');ga('send','pageview');
+	</script>
+
+	<!-- build:js scripts/main.js -->
+	<script data-main="scripts/main" src="bower_components/requirejs/require.js"></script>
+	<!-- endbuild -->
+</body>
+</html>
---
+++ phpProject/scripts/main.js
@@ -0,0 +1,42 @@
+require.config({
+	paths: {
+		jquery: '../bower_components/jquery/jquery.min',
+		bootstrap: '../bower_components/bootstrap/docs/assets/js/bootstrap.min'
+	},
+	shim: { 'bootstrap': { deps: ['jquery'] } },
+	exports: {
+		login: '../scripts/login'
+	}
+});
+
+require(['app', 'jquery', 'bootstrap'], function (app, $, bootstrap) {
+	'use strict';
+	// use app here
+	// console.log(app);
+
+	var redirectPage = function () {
+		if (localStorage.getItem('redirect') === null) {
+			localStorage.setItem('redirect', 'true');
+			window.location.href = '/moodle-project-sumare/phpProject/login.php';
+		};
+	}();
+
+	var sidebarActive = function () {
+		$('.sidebar-teacher ul li').on('click', function () {
+			if($(this).hasClass('nav-header')) return false;
+			$('.sidebar-teacher ul li').removeClass('active');
+			$(this).addClass('active');
+		});
+	}();
+
+	$('#submitLogin').on('click', function () {
+		var user = $('#username'),
+			pass = $('#password');
+
+		if (user.val() !== '' || pass.val() !== '') {
+			alert('não tenho o  que faze');
+		} else {
+			window.location = 'restricted_teacher.php';
+		}
+	});
+});
