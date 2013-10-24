d3f3f14a2e50db712eec006adc7d692ca8d0eef0
rafaellucio <rafaelantoniolucio@gmail.com>
2013-10-23 22:49:13 -0200

    creating chat

--- server/app.js
+++ server/app.js
@@ -3,18 +3,15 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	io = require('socket.io'),
+	app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -27,8 +24,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
